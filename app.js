3f44335c4b0a00bf77800526b22e5af286a0884b
Marlon Agno <mdj.agno@gmail.com>
2019-09-27 15:17:32 -0400

    Revert "first commit and made starting changes for deployment to moviedex-api"
    This reverts commit 8000ecbbb3d2e5790b8e5fc69df4f131ccc16207.

--- app.js
+++ app.js
@@ -1,61 +1,45 @@
-'use strict';
-
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const app = express();
 
-const movies = require('./movieStore');
+const movies = require('./movieStore')
 
 app.use(helmet());
 app.use(cors());
 app.use(function validateBearerToken(req, res, next) {
-  const authToken = req.get('Authorization');
-  const apiToken = process.env.API_TOKEN;
+    console.log('validate bearer token middleware')
+    const authToken = req.get('Authorization');
+    const apiToken = process.env.API_TOKEN
 
-  if (!authToken || authToken.split(' ')[1] !== apiToken){
-    return res.status(401).json({error: 'Unauthorized request'});
-  }
+    if (!authToken || authToken.split(' ')[1] !== apiToken){
+        return res.status(401).json({error: 'Unauthorized request'})
+    }
 
-  next();
-});
-
-app.use((error, req, res, next) => {
-  let response;
-  if (process.env.NODE_ENV === 'production') {
-    response = { error: { message: 'server error' }}
-  } else {
-    response = { error };
-  }
-  res.status(500).json(response);
-});
-
+    next();
+})
 
 //users can search for movies by genre, country, or avg_vote
 
 app.get('/movie', (req, res) => {
-  const {genre, country, avg_vote} = req.query;
+    const {genre, country, avg_vote} = req.query
 
-  let newMovies;
+    let newMovies;
 
-  if (genre){
-    newMovies = movies.filter(movie => movie.genre.toLowerCase().includes(genre.toLowerCase()));
-  }
+    if (genre){
+        newMovies = movies.filter(movie => movie.genre.toLowerCase().includes(genre.toLowerCase()));
+    }
 
-  if (country){
-    newMovies = movies.filter(movie => movie.country.toLowerCase().includes(country.toLowerCase()));
-  }
+    if (country){
+        newMovies = movies.filter(movie => movie.country.toLowerCase().includes(country.toLowerCase()));
+    }
 
-  if (avg_vote){
-    newMovies = movies.filter(movie =>  movie.avg_vote >= parseInt(avg_vote));
-  }
+    if (avg_vote){
+        newMovies = movies.filter(movie =>  movie.avg_vote >= parseInt(avg_vote));
+    }
     
-  res.json(newMovies);
+    res.json(newMovies);
 });
 
-const PORT = process.env.port || 8000;
-
-app.listen(8080, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen(8080, () => {console.log('Server is running on 8080')});
